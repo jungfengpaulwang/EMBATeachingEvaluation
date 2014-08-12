86b9f2913a4765f8c36443bd9a8fcf7c6ea23a34
jungfengpaulwang <paul.wang@ischool.com.tw>
2014-08-12 09:00:35 +0800

    no message

--- Forms/CS_Template_NoSubject.Designer.cs
+++ Forms/CS_Template_NoSubject.Designer.cs
@@ -28,145 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.colorPickerDropDown1 = new DevComponents.DotNetBar.ColorPickerDropDown();
-            this.panelButtom = new DevComponents.DotNetBar.PanelEx();
-            this.btnExit = new DevComponents.DotNetBar.ButtonX();
-            this.btnSave = new DevComponents.DotNetBar.ButtonX();
-            this.panelContent = new DevComponents.DotNetBar.PanelEx();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.panel1 = new DevComponents.DotNetBar.PanelEx();
-            this.bar1 = new DevComponents.DotNetBar.Bar();
-            this.panelButtom.SuspendLayout();
-            this.panelContent.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bar1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // colorPickerDropDown1
-            // 
-            this.colorPickerDropDown1.AutoExpandOnClick = true;
-            this.colorPickerDropDown1.Name = "colorPickerDropDown1";
-            this.colorPickerDropDown1.Text = "A";
-            this.colorPickerDropDown1.SelectedColorChanged += new System.EventHandler(this.colorPickerDropDown1_SelectedColorChanged);
-            // 
-            // panelButtom
-            // 
-            this.panelButtom.CanvasColor = System.Drawing.SystemColors.Control;
-            this.panelButtom.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.panelButtom.Controls.Add(this.btnExit);
-            this.panelButtom.Controls.Add(this.btnSave);
-            this.panelButtom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelButtom.Location = new System.Drawing.Point(0, 519);
-            this.panelButtom.Name = "panelButtom";
-            this.panelButtom.Size = new System.Drawing.Size(784, 42);
-            this.panelButtom.Style.Alignment = System.Drawing.StringAlignment.Center;
-            this.panelButtom.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
-            this.panelButtom.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
-            this.panelButtom.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
-            this.panelButtom.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
-            this.panelButtom.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
-            this.panelButtom.Style.GradientAngle = 90;
-            this.panelButtom.TabIndex = 9;
-            // 
-            // btnExit
-            // 
-            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnExit.Location = new System.Drawing.Point(688, 9);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 25);
-            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "離開";
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // btnSave
-            // 
-            this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnSave.Location = new System.Drawing.Point(596, 9);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 25);
-            this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.btnSave.TabIndex = 2;
-            this.btnSave.Text = "儲存";
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // panelContent
-            // 
-            this.panelContent.CanvasColor = System.Drawing.SystemColors.Control;
-            this.panelContent.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.panelContent.Controls.Add(this.webBrowser1);
-            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(0, 38);
-            this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(784, 481);
-            this.panelContent.Style.Alignment = System.Drawing.StringAlignment.Center;
-            this.panelContent.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
-            this.panelContent.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
-            this.panelContent.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
-            this.panelContent.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
-            this.panelContent.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
-            this.panelContent.Style.GradientAngle = 90;
-            this.panelContent.TabIndex = 10;
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.colorPickerDropDown1 = new DevComponents.DotNetBar.ColorPickerDropDown();
+			this.panelButtom = new DevComponents.DotNetBar.PanelEx();
+			this.btnExit = new DevComponents.DotNetBar.ButtonX();
+			this.btnSave = new DevComponents.DotNetBar.ButtonX();
+			this.panelContent = new DevComponents.DotNetBar.PanelEx();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.panel1 = new DevComponents.DotNetBar.PanelEx();
+			this.bar1 = new DevComponents.DotNetBar.Bar();
+			this.panelButtom.SuspendLayout();
+			this.panelContent.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.bar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// colorPickerDropDown1
+			// 
+			this.colorPickerDropDown1.AutoExpandOnClick = true;
+			this.colorPickerDropDown1.Name = "colorPickerDropDown1";
+			this.colorPickerDropDown1.Text = "A";
+			this.colorPickerDropDown1.SelectedColorChanged += new System.EventHandler(this.colorPickerDropDown1_SelectedColorChanged);
+			// 
+			// panelButtom
+			// 
+			this.panelButtom.CanvasColor = System.Drawing.SystemColors.Control;
+			this.panelButtom.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.panelButtom.Controls.Add(this.btnExit);
+			this.panelButtom.Controls.Add(this.btnSave);
+			this.panelButtom.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelButtom.Location = new System.Drawing.Point(0, 519);
+			this.panelButtom.Name = "panelButtom";
+			this.panelButtom.Size = new System.Drawing.Size(784, 42);
+			this.panelButtom.Style.Alignment = System.Drawing.StringAlignment.Center;
+			this.panelButtom.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+			this.panelButtom.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+			this.panelButtom.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+			this.panelButtom.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+			this.panelButtom.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+			this.panelButtom.Style.GradientAngle = 90;
+			this.panelButtom.TabIndex = 9;
+			// 
+			// btnExit
+			// 
+			this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+			this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+			this.btnExit.Location = new System.Drawing.Point(688, 9);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 25);
+			this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.btnExit.TabIndex = 3;
+			this.btnExit.Text = "離開";
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnSave
+			// 
+			this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+			this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+			this.btnSave.Location = new System.Drawing.Point(596, 9);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 25);
+			this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.btnSave.TabIndex = 2;
+			this.btnSave.Text = "儲存";
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// panelContent
+			// 
+			this.panelContent.CanvasColor = System.Drawing.SystemColors.Control;
+			this.panelContent.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.panelContent.Controls.Add(this.webBrowser1);
+			this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelContent.Location = new System.Drawing.Point(0, 38);
+			this.panelContent.Name = "panelContent";
+			this.panelContent.Size = new System.Drawing.Size(784, 481);
+			this.panelContent.Style.Alignment = System.Drawing.StringAlignment.Center;
+			this.panelContent.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+			this.panelContent.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+			this.panelContent.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+			this.panelContent.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+			this.panelContent.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+			this.panelContent.Style.GradientAngle = 90;
+			this.panelContent.TabIndex = 10;
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.webBrowser1.Location = new System.Drawing.Point(22, 4);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(741, 471);
-            this.webBrowser1.TabIndex = 5;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.bar1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(784, 38);
-            this.panel1.TabIndex = 4;
-            // 
-            // bar1
-            // 
-            this.bar1.AntiAlias = true;
-            this.bar1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bar1.DockSide = DevComponents.DotNetBar.eDockSide.Top;
-            this.bar1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+			this.webBrowser1.Location = new System.Drawing.Point(22, 4);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(741, 471);
+			this.webBrowser1.TabIndex = 5;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.bar1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(784, 38);
+			this.panel1.TabIndex = 4;
+			// 
+			// bar1
+			// 
+			this.bar1.AntiAlias = true;
+			this.bar1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.bar1.DockSide = DevComponents.DotNetBar.eDockSide.Top;
+			this.bar1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.colorPickerDropDown1});
-            this.bar1.Location = new System.Drawing.Point(0, 0);
-            this.bar1.Name = "bar1";
-            this.bar1.Size = new System.Drawing.Size(784, 26);
-            this.bar1.Stretch = true;
-            this.bar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.bar1.TabIndex = 0;
-            this.bar1.TabStop = false;
-            this.bar1.Text = "bar1";
-            // 
-            // CS_Template_NoSubject
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.panelContent);
-            this.Controls.Add(this.panelButtom);
-            this.Controls.Add(this.panel1);
-            this.DoubleBuffered = true;
-            this.MaximizeBox = true;
-            this.Name = "CS_Template_NoSubject";
-            this.Text = "編輯「評鑑注意事項」內容文字樣版";
-            this.Load += new System.EventHandler(this.Email_Content_Template_Load);
-            this.panelButtom.ResumeLayout(false);
-            this.panelContent.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.bar1)).EndInit();
-            this.ResumeLayout(false);
+			this.bar1.Location = new System.Drawing.Point(0, 0);
+			this.bar1.Name = "bar1";
+			this.bar1.Size = new System.Drawing.Size(784, 26);
+			this.bar1.Stretch = true;
+			this.bar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.bar1.TabIndex = 0;
+			this.bar1.TabStop = false;
+			this.bar1.Text = "bar1";
+			// 
+			// CS_Template_NoSubject
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.panelContent);
+			this.Controls.Add(this.panelButtom);
+			this.Controls.Add(this.panel1);
+			this.DoubleBuffered = true;
+			this.MaximizeBox = true;
+			this.Name = "CS_Template_NoSubject";
+			this.Text = "編輯「評鑑注意事項」內容文字樣版";
+			this.Load += new System.EventHandler(this.Email_Content_Template_Load);
+			this.panelButtom.ResumeLayout(false);
+			this.panelContent.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.bar1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
